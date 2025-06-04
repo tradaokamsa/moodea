3a2c8cb5418d75c0ba7e385d054c2163e9c01c5f
tradaokamsa <hainamdao05@gmail.com>
2025-06-04 23:49:51 +0800

    integrated Spotify oauth 2.0

---
+++ backend/src/services/spotifyService.js
@@ -0,0 +1,84 @@
+const axios = require('axios');
+const crypto = require('crypto');
+
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+
+const generateRandomString = (length) => {
+  return crypto
+  .randomBytes(60)
+  .toString('hex')
+  .slice(0, length);
+}
+
+const getAuthUrl = () => {
+  const state = generateRandomString(16);
+  const scopes = [
+    "user-top-read",
+    "user-read-email",
+    "user-read-private",
+    "user-library-read",
+    "user-library-modify",
+    "playlist-read-private",
+    "playlist-modify-public",
+    "playlist-modify-private",
+    "user-modify-playback-state"
+  ];
+  const params = new URLSearchParams({
+    client_id: SPOTIFY_CLIENT_ID,
+    redirect_uri: SPOTIFY_REDIRECT_URI,
+    response_type: 'code',
+    state: state,
+    scope: scopes.join(' ')
+  });
+
+  return `https://accounts.spotify.com/authorize?${params.toString()}`;
+};
+
+const getTokens = async (code) => {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'authorization_code');
+  params.append('code', code);
+  params.append('redirect_uri', SPOTIFY_REDIRECT_URI);
+
+  const response = await axios.post('https://accounts.spotify.com/api/token', params, {
+    headers: {
+      'Authorization': `Basic ${Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64')}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+
+  return response.data;
+};
+
+const refreshAccessToken = async (refreshToken) => {
+  const params = new URLSearchParams();
+  params.append('grant_type', 'refresh_token');
+  params.append('refresh_token', refreshToken);
+
+  const response = await axios.post('https://accounts.spotify.com/api/token', params, {
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'Authorization': `Basic ${Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64')}`
+    }
+  });
+
+  return response.data;
+};
+
+const getUserProfile = async (accessToken) => {
+  const response = await axios.get('https://api.spotify.com/v1/me', {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`
+    }
+  });
+  return response.data;
+};
+
+module.exports = {
+  getAuthUrl,
+  getTokens,
+  refreshAccessToken,
+  getUserProfile
+}; 
